2acea7beb22ee9e72afa594192377eef9f3ef09d
Aaron <asintayehu@proton.me>
2025-02-20 15:04:50 -0500

    feat: Set up flask migrations, resolved database file path issues, fixed database connection problem, added creation of books to data base feature

--- app.py
+++ app.py
@@ -1,28 +1,48 @@
 # Core dependencies
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import *
+from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
 
 # Initializing app, database
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bookie.db"
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-class Book():
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    author = db.Column(db.String(100), nullable=False)
-    genre = db.Column(db.String(100), nullable=True)
+    author = db.Column(db.String(100), nullable=False, default="")
+    genre = db.Column(db.String(100), nullable=True, default="")
     date_created = db.Column(db.DateTime, default= lambda : datetime.now(timezone.utc))
 
+    def __repr__(self):
+        return '<Book %r>' % self.id
+
+
 # estbalish routes
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        book_title = request.form['add-book-name']
+
+        book_title = request.form.get('book')
+        new_book = Book(title=book_title)
+
+        try:
+            db.session.add(new_book)
+            db.session.commit()
+
+            # Query the database
+            Books = Book.query.order_by(Book.date_created).all()
+            return redirect(url_for('index'))
+        except Exception as e:
+            return f'{str(e)}'
 
     else:
-        return render_template('index.html')
+        Books = Book.query.order_by(Book.date_created).all()
+        return render_template('index.html', book_list=Books)
     
     # want attribute and wanna add that to the 
     # table or at least display it on the page in raw html
