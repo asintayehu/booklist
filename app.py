4930b0f96fe02aa762f6e2ed1fd538cff1303f83
Aaron <asintayehu@proton.me>
2025-02-22 11:48:58 -0500

    Implement delete function for book removal; begin API implementation for book lookup

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import *
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
+import requests as rq
 
 # Initializing app, database
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bookie.db"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# iterator list
+numbers = list(range(0,30))
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,9 +30,19 @@ class Book(db.Model):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-
+        # Creation of books
         book_title = request.form.get('book')
         new_book = Book(title=book_title)
+
+        # String parser
+        cleaned_title = new_book.title.replace(" ", "+").lower()
+        # convert to lower case and replace all spaces with a +
+
+
+        url = f'https://openlibrary.org/search.json?q={cleaned_title}'
+        book_request = rq.get(url)
+        print(book_request.json())
+        
 
         try:
             db.session.add(new_book)
@@ -42,10 +56,25 @@ def index():
 
     else:
         Books = Book.query.order_by(Book.date_created).all()
-        return render_template('index.html', book_list=Books)
-    
-    # want attribute and wanna add that to the 
-    # table or at least display it on the page in raw html
+        return render_template('index.html',   packed=zip(Books, numbers))
+
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+    if request.method == 'POST':
+        # Grab id for book
+        target_book = db.get_or_404(Book, id)
+
+        try:
+            db.session.delete(target_book)
+            db.session.commit()
+        except Exception as e:
+            return f'{str(e)}'
+        return redirect('/')
+    else:
+        return redirect('/')
+
 
 if __name__ == "__main__":
+
     app.run(debug=True)
