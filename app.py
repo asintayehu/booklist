3c6c5768a6c1ec29a1ca2cc0a980436f469d9dbc
Aaron <asintayehu@proton.me>
2025-02-27 19:10:41 -0500

    Added user authentication features, will finish tomorrow

--- app.py
+++ app.py
@@ -6,15 +6,43 @@ from datetime import *
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 import requests as rq
+from flask_login import LoginManager, UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+import secrets
 
 # Initializing app, database
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bookie.db"
+app.config['SECRET_KEY'] = ''
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
+
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 # iterator list
 numbers = list(range(0,30))
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False)
+    bookshelf = db.relationship('Book', backref='User', lazy='dynamic')
+
+    # is_authenticated
+    # is_active
+    # is_anonymous
+    # get_id
+
+    def __repr__(self):
+        return '<Username: %s>' % self.id
+
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,6 +50,7 @@ class Book(db.Model):
     author = db.Column(db.String(100), nullable=False, default="")
     genre = db.Column(db.String(100), nullable=True, default="")
     date_created = db.Column(db.DateTime, default= lambda : datetime.now(timezone.utc))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     rating = db.Column(db.Double, nullable=False)
 
     __table_args__ = (
@@ -31,6 +60,30 @@ class Book(db.Model):
     def __repr__(self):
         return '<Book %r>' % self.id
 
+class RegisterForm(FlaskForm):
+    user_name = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+
+    # Query database
+    def validate_name(self, user_name):
+        with app.app_context():
+            existing_name = User.query.filter_by(user_name=user_name.data).first()
+            if existing_name:
+                raise ValidationError ("User already exists")
+
+    # existing_name = User.query.filter_by(user_name=user_name.data).first()
+    # if existing_name:
+    #     raise ValidationError ("User already exists")
+    
+class LoginForm(FlaskForm):
+    user_name = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # estbalish routes
 @app.route('/', methods=['GET', 'POST'])
@@ -52,7 +105,6 @@ def index():
         # book_request = rq.get(url)
         # print(book_request.json())
         
-
         try:
             db.session.add(new_book)
             db.session.commit()
@@ -99,8 +151,30 @@ def update(id):
         return redirect('/')
     except Exception as e:
         return f'{str(e)}'
+    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(user_name=form.user_name.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(user_name=form.user_name.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
